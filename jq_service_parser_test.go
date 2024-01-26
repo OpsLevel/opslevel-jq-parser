47b6b63a385b87f5d3bbdc1e7979925b11c0f64a
Taimoor Ahmad <taimoor@opslevel.com>
2024-01-26 11:03:47 -0500

    wip - map to json string instead of propertyinput obj

--- jq_service_parser_test.go
+++ jq_service_parser_test.go
@@ -22,9 +22,9 @@ var k8sResource = `{
 			"opslevel.com/lifecycle": "alpha",
 			"opslevel.com/tier": "tier_1",
 			"opslevel.com/product": "jklabs",
-			"opslevel.com/property.my_bool": "{\"owner\": \"platform\", \"value\": true}",
-            "opslevel.com/property.my_string": "{\"owner\": \"leadership\", \"value\": \"hello world\"}",
-            "opslevel.com/property.my_object": "{\"owner\": \"engineering\", \"value\": {\"message\": \"hello world\", \"boolean\": true}}",
+			"opslevel.com/property.my_bool": "true",
+            "opslevel.com/property.my_string": "\"hello world\"",
+            "opslevel.com/property.my_object": "{\"message\": \"hello world\", \"boolean\": true}",
 			"opslevel.com/language": "ruby",
 			"opslevel.com/framework": "rails",
 			"opslevel.com/system": "monolith",
@@ -308,17 +308,15 @@ func TestJQServiceParserSampleConfig(t *testing.T) {
 	autopilot.Equals(t, 4, len(service.Tools))
 	autopilot.Equals(t, 3, len(service.Repositories))
 	// property assignment
+	fmt.Println(service.Properties)
 	autopilot.Equals(t, 3, len(service.Properties))
-	autopilot.Equals(t, "platform", *service.Properties[0].Owner.Alias)
-	autopilot.Equals(t, "leadership", *service.Properties[1].Owner.Alias)
-	autopilot.Equals(t, "engineering", *service.Properties[2].Owner.Alias)
-	autopilot.Equals(t, true, service.Properties[0].Value.AsBool())
-	autopilot.Equals(t, "hello world", service.Properties[1].Value.AsString())
+	autopilot.Equals(t, true, service.Properties["my_bool"].AsBool())
+	autopilot.Equals(t, "hello world", service.Properties["my_string"].AsString())
 	expectedValue := map[string]interface{}{
 		"boolean": true,
 		"message": "hello world",
 	}
-	autopilot.Equals(t, expectedValue, service.Properties[2].Value.AsMap())
+	autopilot.Equals(t, expectedValue, service.Properties["my_object"].AsMap())
 }
 
 func BenchmarkJQParser_New(b *testing.B) {
