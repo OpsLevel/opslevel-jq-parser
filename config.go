491a2c68c9f6cfa582079eb717c5a98a90d32f30
Taimoor Ahmad <taimoor@opslevel.com>
2024-01-30 13:47:48 -0500

    Add support for parsing property assignments (JSON support) (#18)

--- config.go
+++ config.go
@@ -9,6 +9,14 @@ var SimpleConfig = `name: .metadata.name
 owner: .metadata.namespace
 aliases: # This are how we identify the services again during reconciliation - please make sure they are very unique
   - '"k8s:\(.metadata.name)-\(.metadata.namespace)"'
+properties:
+  notfound: .metadata.annotations.notfound
+  notfound2: .metadata.annotations.notfound2
+  prop_bool: .metadata.annotations.prop_bool
+  prop_empty_object: .metadata.annotations.prop_empty_object
+  prop_empty_string: .metadata.annotations.prop_empty_string
+  prop_object: .metadata.annotations.prop_object
+  prop_string: .metadata.annotations.prop_string
 tags:
   assign: # tag with the same key name but with a different value will be updated on the service
     - '{"imported": "kubectl-opslevel"}'
@@ -18,21 +26,27 @@ tags:
 `
 
 var SampleConfig = `name: .metadata.name
-description: .metadata.annotations."opslevel.com/description"
-owner: .metadata.annotations."opslevel.com/owner"
-lifecycle: .metadata.annotations."opslevel.com/lifecycle"
-tier: .metadata.annotations."opslevel.com/tier"
-product: .metadata.annotations."opslevel.com/product"
-language: .metadata.annotations."opslevel.com/language"
-framework: .metadata.annotations."opslevel.com/framework"
-system: .metadata.annotations."opslevel.com/system"
 aliases: # This are how we identify the services again during reconciliation - please make sure they are very unique
   - '"k8s:\(.metadata.name)-\(.metadata.namespace)"'
   - '"\(.metadata.namespace)-\(.metadata.name)"'
+description: .metadata.annotations."opslevel.com/description"
+framework: .metadata.annotations."opslevel.com/framework"
+language: .metadata.annotations."opslevel.com/language"
+lifecycle: .metadata.annotations."opslevel.com/lifecycle"
+owner: .metadata.annotations."opslevel.com/owner"
+product: .metadata.annotations."opslevel.com/product"
+system: .metadata.annotations."opslevel.com/system"
+tier: .metadata.annotations."opslevel.com/tier"
+repositories: # attach repositories to the service using the opslevel repo alias - IE github.com:hashicorp/vault
+  - '{"name": "My Cool Repo", "directory": "/", "repo": .metadata.annotations.repo} | if .repo then . else empty end'
+  # if just the alias is returned as a single string we'll build the name for you and set the directory to "/"
+  - .metadata.annotations.repo
+  # find annotations with format: opslevel.com/repo.<displayname>.<repo.subpath.dots.turned.to.forwardslash>: <opslevel repo alias>
+  - '.metadata.annotations | to_entries |  map(select(.key | startswith("opslevel.com/repo"))) | map({"name": .key | split(".")[2], "directory": .key | split(".")[3:] | join("/"), "repo": .value})'
 tags:
   assign: # tag with the same key name but with a different value will be updated on the service
     - '{"imported": "kubectl-opslevel"}'
-    # find annoations with format: opslevel.com/tags.<key name>: <value>
+    # find annotations with format: opslevel.com/tags.<key name>: <value>
     - '.metadata.annotations | to_entries |  map(select(.key | startswith("opslevel.com/tags"))) | map({(.key | split(".")[2]): .value})'
     - .metadata.labels
   create: # tag with the same key name but with a different value with be added to the service
@@ -43,12 +57,6 @@ tools:
   - '.metadata.annotations | to_entries |  map(select(.key | startswith("opslevel.com/tools"))) | map({"category": .key | split(".")[2], "displayName": .key | split(".")[3], "url": .value})'
   # OR find annotations with format: opslevel.com/tools.<category>.<environment>.<displayname>: <url>
   # - '.metadata.annotations | to_entries |  map(select(.key | startswith("opslevel.com/tools"))) | map({"category": .key | split(".")[2], "environment": .key | split(".")[3], "displayName": .key | split(".")[4], "url": .value})'
-repositories: # attach repositories to the service using the opslevel repo alias - IE github.com:hashicorp/vault
-  - '{"name": "My Cool Repo", "directory": "/", "repo": .metadata.annotations.repo} | if .repo then . else empty end'
-  # if just the alias is returned as a single string we'll build the name for you and set the directory to "/"
-  - .metadata.annotations.repo
-  # find annotations with format: opslevel.com/repo.<displayname>.<repo.subpath.dots.turned.to.forwardslash>: <opslevel repo alias>
-  - '.metadata.annotations | to_entries |  map(select(.key | startswith("opslevel.com/repo"))) | map({"name": .key | split(".")[2], "directory": .key | split(".")[3:] | join("/"), "repo": .value})'
 `
 
 type TagRegistrationConfig struct {
@@ -58,37 +66,39 @@ type TagRegistrationConfig struct {
 
 // ServiceRegistrationConfig represents the jq expressions configuration that can turn json data into a ServiceRegistration
 type ServiceRegistrationConfig struct {
-	Name         string                `json:"name" yaml:"name"`
-	Description  string                `json:"description" yaml:"description"`
-	Owner        string                `json:"owner" yaml:"owner"`
-	Lifecycle    string                `json:"lifecycle" yaml:"lifecycle"`
-	Tier         string                `json:"tier" yaml:"tier"`
-	Product      string                `json:"product" yaml:"product"`
-	Language     string                `json:"language" yaml:"language"`
-	Framework    string                `json:"framework" yaml:"framework"`
-	System       string                `json:"system" yaml:"system"`
 	Aliases      []string              `json:"aliases" yaml:"aliases"` // JQ expressions that return a single string or a []string
-	Tags         TagRegistrationConfig `json:"tags" yaml:"tags"`
-	Tools        []string              `json:"tools" yaml:"tools"`               // JQ expressions that return a single map[string]string or a []map[string]string
+	Description  string                `json:"description" yaml:"description"`
+	Framework    string                `json:"framework" yaml:"framework"`
+	Language     string                `json:"language" yaml:"language"`
+	Lifecycle    string                `json:"lifecycle" yaml:"lifecycle"`
+	Name         string                `json:"name" yaml:"name"`
+	Owner        string                `json:"owner" yaml:"owner"`
+	Product      string                `json:"product" yaml:"product"`
+	Properties   map[string]string     `json:"properties" yaml:"properties"`
 	Repositories []string              `json:"repositories" yaml:"repositories"` // JQ expressions that return a single string or []string or map[string]string or a []map[string]string
+	System       string                `json:"system" yaml:"system"`
+	Tags         TagRegistrationConfig `json:"tags" yaml:"tags"`
+	Tier         string                `json:"tier" yaml:"tier"`
+	Tools        []string              `json:"tools" yaml:"tools"` // JQ expressions that return a single map[string]string or a []map[string]string
 }
 
 // ServiceRegistration represents the parsed json data from a ServiceRegistrationConfig
 type ServiceRegistration struct {
-	Name         string                                  `json:",omitempty"`
-	Description  string                                  `json:",omitempty"`
-	Owner        string                                  `json:",omitempty"`
-	Lifecycle    string                                  `json:",omitempty"`
-	Tier         string                                  `json:",omitempty"`
-	Product      string                                  `json:",omitempty"`
-	Language     string                                  `json:",omitempty"`
-	Framework    string                                  `json:",omitempty"`
-	System       string                                  `json:",omitempty"`
 	Aliases      []string                                `json:",omitempty"`
+	Description  string                                  `json:",omitempty"`
+	Framework    string                                  `json:",omitempty"`
+	Language     string                                  `json:",omitempty"`
+	Lifecycle    string                                  `json:",omitempty"`
+	Name         string                                  `json:",omitempty"`
+	Owner        string                                  `json:",omitempty"`
+	Product      string                                  `json:",omitempty"`
+	Properties   map[string]string                       `json:",omitempty"`
+	Repositories []opslevel.ServiceRepositoryCreateInput `json:",omitempty"` // This is a concrete class so fields are validated during `service preview`
+	System       string                                  `json:",omitempty"`
 	TagAssigns   []opslevel.TagInput                     `json:",omitempty"`
 	TagCreates   []opslevel.TagInput                     `json:",omitempty"`
+	Tier         string                                  `json:",omitempty"`
 	Tools        []opslevel.ToolCreateInput              `json:",omitempty"` // This is a concrete class so fields are validated during `service preview`
-	Repositories []opslevel.ServiceRepositoryCreateInput `json:",omitempty"` // This is a concrete class so fields are validated during `service preview`
 }
 
 func NewServiceRegistrationConfig(data string) (*ServiceRegistrationConfig, error) {
---
+++ jq_dict_parser.go
@@ -0,0 +1,46 @@
+package opslevel_jq_parser
+
+import (
+	"fmt"
+
+	libjq_go "github.com/flant/libjq-go"
+	"github.com/rs/zerolog/log"
+)
+
+type JQDictParser map[string]JQFieldParser
+
+func NewJQDictParser(dict map[string]string) map[string]JQFieldParser {
+	output := make(map[string]JQFieldParser)
+	if dict == nil {
+		return output
+	}
+	jq := libjq_go.Jq()
+	for key, expression := range dict {
+		prg, err := jq.Program(expression).Precompile()
+		if err != nil {
+			panic(fmt.Sprintf("unable to compile jq dict: %s", dict))
+		}
+		output[key] = JQFieldParser{
+			program: prg,
+		}
+	}
+	return output
+}
+
+func (p JQDictParser) Run(data string) (map[string]string, error) {
+	output := make(map[string]string)
+	for key, expression := range p {
+		jqRes, err := expression.Run(data)
+		if err != nil {
+			log.Warn().Str("key", key).Err(err).Msg("error running jq expression")
+			continue
+		}
+		if jqRes == "null" {
+			// in the case that the expression returned nothing (happens in the case where the key was not found)
+			// jq will return "null". This is not the same as empty string. So in that case, skip the item.
+			continue
+		}
+		output[key] = jqRes
+	}
+	return output, nil
+}
