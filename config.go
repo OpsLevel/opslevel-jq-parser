9af02d88bf0a1841f9d072c2788b4015cbae4d65
Taimoor Ahmad <taimoor@opslevel.com>
2024-03-08 20:36:24 -0500

    use go embed to store files, test cases

--- config.go
+++ config.go
@@ -5,60 +5,6 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-var SimpleConfig = `name: .metadata.name
-owner: .metadata.namespace
-aliases: # This are how we identify the services again during reconciliation - please make sure they are very unique
-  - '"k8s:\(.metadata.name)-\(.metadata.namespace)"'
-properties:
-  notfound: .metadata.annotations.notfound
-  notfound2: .metadata.annotations.notfound2
-  prop_bool: .metadata.annotations.prop_bool
-  prop_empty_object: .metadata.annotations.prop_empty_object
-  prop_empty_string: .metadata.annotations.prop_empty_string
-  prop_object: .metadata.annotations.prop_object
-  prop_string: .metadata.annotations.prop_string
-tags:
-  assign: # tag with the same key name but with a different value will be updated on the service
-    - '{"imported": "kubectl-opslevel"}'
-    - .metadata.labels
-  create: # tag with the same key name but with a different value with be added to the service
-    - '{"environment": .spec.template.metadata.labels.environment}'
-`
-
-var SampleConfig = `name: .metadata.name
-aliases: # This are how we identify the services again during reconciliation - please make sure they are very unique
-  - '"k8s:\(.metadata.name)-\(.metadata.namespace)"'
-  - '"\(.metadata.namespace)-\(.metadata.name)"'
-description: .metadata.annotations."opslevel.com/description"
-framework: .metadata.annotations."opslevel.com/framework"
-language: .metadata.annotations."opslevel.com/language"
-lifecycle: .metadata.annotations."opslevel.com/lifecycle"
-owner: .metadata.annotations."opslevel.com/owner"
-product: .metadata.annotations."opslevel.com/product"
-system: .metadata.annotations."opslevel.com/system"
-tier: .metadata.annotations."opslevel.com/tier"
-repositories: # attach repositories to the service using the opslevel repo alias - IE github.com:hashicorp/vault
-  - '{"name": "My Cool Repo", "directory": "", "repo": .metadata.annotations.repo} | if .repo then . else empty end'
-  # if just the alias is returned as a single string we'll build the name for you and set the directory to "/"
-  - .metadata.annotations.repo
-  # find annotations with format: opslevel.com/repo.<displayname>.<repo.subpath.dots.turned.to.forwardslash>: <opslevel repo alias>
-  - '.metadata.annotations | to_entries |  map(select(.key | startswith("opslevel.com/repo"))) | map({"name": .key | split(".")[2], "directory": .key | split(".")[3:] | join("/"), "repo": .value})'
-tags:
-  assign: # tag with the same key name but with a different value will be updated on the service
-    - '{"imported": "kubectl-opslevel"}'
-    # find annotations with format: opslevel.com/tags.<key name>: <value>
-    - '.metadata.annotations | to_entries |  map(select(.key | startswith("opslevel.com/tags"))) | map({(.key | split(".")[2]): .value})'
-    - .metadata.labels
-  create: # tag with the same key name but with a different value with be added to the service
-    - '{"environment": .spec.template.metadata.labels.environment}'
-tools:
-  - '{"category": "other", "environment": "production", "displayName": "my-cool-tool", "url": .metadata.annotations."example.com/my-cool-tool"} | if .url then . else empty end'
-  # find annotations with format: opslevel.com/tools.<category>.<displayname>: <url>
-  - '.metadata.annotations | to_entries |  map(select(.key | startswith("opslevel.com/tools"))) | map({"category": .key | split(".")[2], "displayName": .key | split(".")[3], "url": .value})'
-  # OR find annotations with format: opslevel.com/tools.<category>.<environment>.<displayname>: <url>
-  # - '.metadata.annotations | to_entries |  map(select(.key | startswith("opslevel.com/tools"))) | map({"category": .key | split(".")[2], "environment": .key | split(".")[3], "displayName": .key | split(".")[4], "url": .value})'
-`
-
 type TagRegistrationConfig struct {
 	Assign []string `json:"assign" yaml:"assign"` // JQ expressions that return a single string or a map[string]string
 	Create []string `json:"create" yaml:"create"` // JQ expressions that return a single string or a map[string]string
