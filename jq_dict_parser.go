bde70f974b0d4b857ccf38275259511ef54417b9
David Bloss <david@opslevel.com>
2024-03-12 14:02:21 -0500

    remove appendEmptyExpr, not needed

--- jq_dict_parser.go
+++ jq_dict_parser.go
@@ -16,7 +16,6 @@ func NewJQDictParser(dict map[string]string) map[string]JQFieldParser {
 	}
 	jq := libjq_go.Jq()
 	for key, expression := range dict {
-		expression = appendEmptyExpr(expression)
 		prg, err := jq.Program(expression).Precompile()
 		if err != nil {
 			panic(fmt.Sprintf("unable to compile jq dict: %s", dict))
--- jq_field_parser.go
+++ jq_field_parser.go
@@ -7,19 +7,11 @@ import (
 	"github.com/flant/libjq-go/pkg/jq"
 )
 
-func appendEmptyExpr(expression string) string {
-	if expression == "" {
-		return "empty"
-	}
-	return expression + " // empty"
-}
-
 type JQFieldParser struct {
 	program *jq.JqProgram
 }
 
 func NewJQFieldParser(expression string) *JQFieldParser {
-	expression = appendEmptyExpr(expression)
 	prg, err := libjq_go.Jq().Program(expression).Precompile()
 	if err != nil {
 		panic(fmt.Sprintf("unable to compile jq expression:  %s", expression))
