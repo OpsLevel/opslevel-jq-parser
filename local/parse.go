3ae3140caea445622829eae19817e9f9da101fb0
David Bloss <david@opslevel.com>
2024-03-12 09:54:13 -0700

    add local parse task (#39)
    * add local parse task
    
    * remove duplicate lines from deployment.json
    
    * wrap up v1 of task parse
    
    * update task parse explanation
    
    * add blank deployment.json
    
    * update task parse
    
    * remove unused variable

---
+++ local/parse.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+
+	opslevel_jq_parser "github.com/opslevel/opslevel-jq-parser/v2024"
+)
+
+//go:embed config.yaml
+var configYamlFile string
+
+func main() {
+	deploymentJson, err := os.ReadFile("../testdata/deployment.json")
+	if err != nil {
+		panic(err)
+	}
+
+	config := getServiceRegistrationConfig()
+	parser := opslevel_jq_parser.NewJQServiceParser(*config)
+	service, err := parser.Run(string(deploymentJson))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(service)
+}
+
+func getServiceRegistrationConfig() *opslevel_jq_parser.ServiceRegistrationConfig {
+	svcRegConfig, err := opslevel_jq_parser.NewServiceRegistrationConfig(configYamlFile)
+	if err != nil {
+		panic(err)
+	}
+	return svcRegConfig
+}
